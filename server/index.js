c0c585f237a77d9e6b54437f49d971d2f9ee0ae1
yube01 <yubraja008@gmail.com>
2024-06-14 17:55:30 +0545

    register login component

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+import authRoute from "./routes/user.route.js"
+
+dotenv.config()
+
+const app = express()
+
+app.use(express.json())
+
+app.use("/auth",authRoute)
+
+app.listen(9000,()=>{
+    console.log("Server started")
+})
+
+mongoose.connect(process.env.MONGO)
+.then(()=>console.log("DB connected"))
+.catch((err)=> console.log(err))
