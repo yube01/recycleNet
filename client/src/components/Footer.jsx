09a9fffd4621ee2200cd877f89bb9bc976365754
Prithvi <prithvighatani7@gmail.com>
2024-06-15 15:35:58 +0545

    conflit rsolve

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-column">
+          <h3 className="footer-text">
+            <span className="green-text">Recycle</span>
+            <span className="black-text">Net</span>
+          </h3>
+          <p>Copyright © 2024. RecycleNet</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
