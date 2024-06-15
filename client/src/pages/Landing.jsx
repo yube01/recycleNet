bfdd2a2cbec7ca08e8cfa49efdd0977d87bfc65f
Prithvi <prithvighatani7@gmail.com>
2024-06-15 10:26:48 +0545

    buy page updates

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -2,17 +2,30 @@ import React from "react";
 import Nav from "../components/Nav";
 import Hero from "../components/Hero";
 import Card from "../components/Card";
+import "./Landing.css";
 
 export default function Landing() {
   return (
     <>
       <Nav />
       <Hero />
-      <Card
-        imagePath="path-to-recycle-icon.png"
-        heading="Sustainability"
-        paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel mauris eget arcu fermentum congue sed sed libero."
-      />
+      <div className="card-container">
+        <Card
+          imagePath="path-to-recycle-icon.png"
+          heading="Sustainability"
+          paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel mauris eget arcu fermentum congue sed sed libero."
+        />
+        <Card
+          imagePath="path-to-recycle-icon.png"
+          heading="Sustainability"
+          paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel mauris eget arcu fermentum congue sed sed libero."
+        />
+        <Card
+          imagePath="path-to-recycle-icon.png"
+          heading="Sustainability"
+          paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel mauris eget arcu fermentum congue sed sed libero."
+        />
+      </div>
     </>
   );
 }
